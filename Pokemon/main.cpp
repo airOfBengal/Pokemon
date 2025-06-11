5bb48e14b428948abed3da1eb5c52531f0d610b3
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-06-11 22:46:53 +0600

    assignment - create main() function

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main() {
+using namespace std;
 
+int main() {
+    string player_name;
+    cout << "Enter Player Name: ";
+    cin >> player_name;
+    cout << "Congratulations, " << player_name << "!\n";
     return 0;
 }
